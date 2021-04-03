8a225daa3ddd20f83d97ae9d04b592e7ad11d1cc
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-04-03 00:30:17 -0700

    update
     anyway.. idk im so tired...

---
+++ static/app.js
@@ -0,0 +1,11 @@
+
+d3.json("../samples.json").then(function(data){
+    console.log(data)
+  });
+  
+
+  //need to figure out how to pull in data...
+  //then want to make traces
+  //then connect them to drop down
+
+  //also want to 
