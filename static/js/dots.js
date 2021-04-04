c49cb14bca11357dc789f0d8e397d3e1e4180b95
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-04-03 23:05:24 -0700

    getting there
    ok, so I am getting there, very slowly, but working on it.. I want to make my start-up plot and then work on the changing plot portion afterward, then I should be done...

--- static/js/dots.js
+++ static/js/dots.js
@@ -1,11 +1,19 @@
 d3.json("../samples.json").then(function(Data){
     var data = Data
+    
+//testing variables created
+    var samples = data.samples[0]
+    console.log(samples.otu_ids);
 
     var names = data.names
-    console.log(names)
+    console.log(names);
     
     var metadata = data.metadata
-    console.log(metadata[0])
+    console.log(metadata[0].ethnicity);
+
+    if (samples.id == 940){
+        console.log(samples.otu_ids)
+    };
 
 //ok, so want to match id from metadata to get information to make bubble graph
 //also subject id needs to match the name
@@ -19,27 +27,63 @@ for(var i = 0; i < names.length; i++) {
     sel.appendChild(opt);
 }
 
-function init() {
-    data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16] }];
-  
-    Plotly.newPlot("bubble", data);
-  }
+//pulling out demo data for 940 start value
+for (var i = 0; i < metadata.length; i++){
+    if (metadata[i].id === 940){
+    demo_data = metadata[i]
+    console.log(demo_data)
+}
 
-//pulling out demographic data
+
+//output onto html
+var output = document.getElementById('sample-metadata')
+output.innerHTML = `<center>ID: ${demo_data.id} 
+                    <br> 
+                    Ethnicity: ${demo_data.ethnicity}
+                    <br>
+                    Gender: ${demo_data.gender} 
+                    <br>
+                    Age: ${demo_data.age} 
+                    <br>
+                    Location: ${demo_data.location} 
+                    <br>
+                    BBType: ${demo_data.bbtype}
+                    <br>
+                    Wfreq: ${demo_data.wfreq}</center>`
+
+ 
+
+//output of graph
+    //     if (data.samples[0].id == 940){
+    //         x_otu_id = data.samples[0].otu_ids
+    //         console.log(x_otu_id)
+    //     // Use sample_values for the y values.
+    //         y_values = data.samples[0].sample_values
+    // var og_trace = {
+    //     type: "scatter",
+    //     name: value,
+    //     x: x_otu_id,
+    //     y: y_values,
+    //   };
+
+    //   var data = [og_trace];
+
+    
+    
+
+  
+Plotly.newPlot("plot", data);
+
+
 
 // Call updatePlotly() when a change takes place to the DOM
 d3.selectAll("#selDataset").on("change", updatePlotly);
 
 
 function updatePlotly() {
-    // Use D3 to select the dropdown menu
-    // var dropdownMenu = d3.select("#selDataset");
-    // // Assign the value of the dropdown menu option to a variable
-    // var value = dropdownMenu.property(this.value)
-    // console.log(value)
-    var value = d3.select("#selDataset").node().value; 
+
+    var dropdownMenu = d3.select("#selDataset");
+    var value = dropdownMenu.node().value;
     console.log(value)
 
     //pulling out demographic data
@@ -47,16 +91,57 @@ function updatePlotly() {
         if (metadata[i].id === value){
             demo_data = metadata[i]
             console.log(demo_data)
+        
 
-        d3.selectAll('#sample-metadata').append(demo_data)
+        var output = document.getElementById('sample-metadata')
+        output.innerHTML = `<center>ID: ${demo_data.id} 
+                            <br> 
+                            Ethnicity: ${demo_data.ethnicity}
+                            <br>
+                            Gender: ${demo_data.gender} 
+                            <br>
+                            Age: ${demo_data.age} 
+                            <br>
+                            Location: ${demo_data.location} 
+                            <br>
+                            BBType: ${demo_data.bbtype}
+                            <br>
+                            Wfreq: ${demo_data.wfreq}</center>`
         }
 
+    // //Use otu_ids for the x values.
+    //     if (value === data.samples[0].id){
+    //         x_otu_id = data.samples[0].otu_ids
+    // // Use sample_values for the y values.
+    //         y_values = data.samples[0].sample_values
+    // // Use sample_values for the marker size.
+            
+    // // Use otu_ids for the marker colors.
+            
+    // // Use otu_labels for the text values.
     
+     }  
+
+            // var trace2 = {
+            //     type: "scatter",
+            //     name: value,
+            //     x: x_otu_id,
+            //     y: y_values,
+            //     line: {
+            //       color: "#17BECF"
+            //     }
+            //   };
+          
+            //   var data = [trace2];
+
 
     }
-}
-Plotly.restyle("bubble", "x", [x]);
-Plotly.restyle("bubble", "y", [y]);
+    }
+// Plotly.restyle("plot", "x", [x]);
+// Plotly.restyle("plot", "y", [y]);
+
 });
 
 init();
+
+
